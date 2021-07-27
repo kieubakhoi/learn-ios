aea5c35731c7063d403228df5c615df362353ada
khoikb <kieubakhoi@gmail.com>
2021-07-27 23:18:44 +0700

    Initial Commit

---
+++ demo-ios-day1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demo-ios-day1
+//
+//  Created by Apple on 27/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
